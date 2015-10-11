26726d205ae120394abede1db99e5ab96dd5254e
unknown <vermarupanshu733@gmai.com>
2015-10-11 16:37:11 +0530

    3rg commit for oqp...

--- bottom-label.php
+++ bottom-label.php
@@ -1,1 +1,3 @@
-<div style="background:grey;width:100%;height:auto;border:2px solid black;font-size:2vw;padding:1%;left:0px;bottom:2px;position:fixed;opacity:0.7;"><center>&copy srs 2015 .</center></div>
+<div style="background:black;width:100%;height:auto;
+border:2px solid white;font-size:2vw;padding:1%;opacity:0.8;
+left:0px;bottom:2px;position:fixed;color:white;"><center>&copy srs 2015 .</center></div>
