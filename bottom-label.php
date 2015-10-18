3c61902c58cfad4dca937ab1c9c6583dbad2a05f
sagarikavishnoi <sagarikavishnoi7@gmail.com>
2015-10-18 10:23:39 +0530

    deleted  Please enter the commit message for your changes. Lines starting

--- bottom-label.php
+++
@@ -1,3 +0,0 @@
-<div style="background:#00796B;width:90%;height:auto;
-font-size:2vw;padding:.6%;
-bottom:2px;position:fixed;color:#FBF5F3;font-family: 'Open Sans Condensed', arial, sans;"><center>&copy srs 2015 .</center></div>
--- configuration_file.php
+++
@@ -1,9 +0,0 @@
-<?php 
-//Database configuration setting files are mentioned here...
-	$server = "localhost";
-	$server_name = "root";
-	$server_password = '';
-	
-	 $connection = mysql_connect($server,$server_name,$server_password) or die("could");
-	 mysql_query("USE OQP");
-?>
