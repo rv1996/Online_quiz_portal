4e92267d6482e4372675c77fd3c47e24eef7efad
sagarikavishnoi <sagarikavishnoi7@gmail.com>
2015-10-24 11:05:08 +0530

    latest change

---
+++ Page-heading.php
@@ -0,0 +1,3 @@
+<div style="background:#00796B;color:#FBF5F3;width:100%;height:auto;font-size:45px;padding:1%;
+position:relative;font-family: 'Open Sans Condensed', arial, sans;"><center>Online Quiz Portal</center></div>
+
---
+++ bottom-label.php
@@ -0,0 +1,3 @@
+<div style="background:#00796B;width:90%;height:auto;
+font-size:2vw;padding:.6%;
+bottom:2px;position:fixed;color:#FBF5F3;font-family: 'Open Sans Condensed', arial, sans;"><center>&copy srs 2015 .</center></div>
