15b7d3f923dd3d53a80c4b43b01c709f9c52c281
unknown <vermarupanshu733@gmai.com>
2015-10-26 00:17:12 +0530

    graphical analysis of student records have made...

--- student_details.php
+++ student_details.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 //student details form... regarding the exam which had been given by the individual student..
 
 //---- all the file equivalent to the templetes are include here....
@@ -7,12 +7,13 @@ include 'bottom-label.php';
 include 'connect.php'  ;
 include 'core.php';
 include 'navigation.php';
+// include 'eaxm_given.php';
 
 
 //---$student_number = //variable to be updated with the session entry....
 
 //---basic SQL uqery
-$sql_query = "SELECT * FROM records  ";
+$sql_query = "SELECT * FROM records ";
 $result = mysql_query($sql_query);
 $result_data = mysql_fetch_array($result);
 
@@ -27,47 +28,51 @@ login_redirect_student();
 <head>
 	<link href="style.css" type="text/css" rel="stylesheet">
 	<script src = "javascript.js"></script>
+	<script src = "ajax.js"></script>
 	<script src="jquery-1.11.3.min.js"></script>
 	<script>
-	
-	function details(){
-		var xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function() {
-		if (xhttp.readyState == 4 && xhttp.status == 200) {
-			document.getElementById('details_data_display').innerHTML = xhttp.responseText;
-		}
-		}
-		xhttp.open('student_graph.php',true);
-		xhttp.send();
-	}
-	
-	
-		
-		
-		
+	var graph_div = document.getElementById('graph');
+	var exam_div = document.getElementById('exam_given');
+
 		//----sliding effect of the JQuery is used....
 		//----for now CDN is not used...instead we have downloaded the coresponding JQuery file in the same Directory...
-		
+
+
 		$(document).ready(function(){
-			
+
 			$("#exam").click(function(){
-				$("#slide-exam").slideToggle("slow");
+				$("#slide-exam").slideToggle("fast");
 			})
-			
-			$("#percent").click(function(){
-				$("#slide-percent").slideToggle("slow");
-			})
-			
+
+
+
 			$("#graph").click(function(){
-				$("#slide-graph").slideToggle("slow");
+				$("#slide-graph").slideToggle("fast");
 			})
-			
-			$(".table_data").click(function(){
-				$("#data1").slideToggle("slow");
-			})
+
+
 		});
+
+		function show_graph(){
+				graph_div.style.display = "block";
+
+		}
+		function hide_graph(){
+					graph_div.style.display = "none";
+		}
 	</script>
+
 	<style>
+		table, th, td {
+    	border: 1px solid black;
+    	border-collapse: collapse;
+		}
+		th, td {
+    	padding: 5px;
+		}
+		th {
+    	text-align: left;
+		}
 		nav{
 			background:#A13549;
 			color:white;
@@ -89,34 +94,71 @@ login_redirect_student();
 			margin-bottom:10px;
 		}
 		nav span{
-			font-size:large;
+			font-size:small;
 			color:white;
 			display:none;
+			padding: 1%;
 			heigth:100px;
 		}
-		
+		#exam_given{
+			margin-top:2%;
+			margin-bottom:2%;
+		}
+
+		}
 	</style>
 	</head>
 
 
 	<body>
-		
-		
+
+
 		<nav id="list">
-				<div id="exam" onclick="return details();">Exam Given</div>
-				<span id="slide-exam">Contain the details about the exam given </span>
-				<div id="percent">Percentile</div>
-				<span id="slide-percent">Contain the details about the percentage and high score</span>
-				<div id="graph">Graph Analysis</div>
-				<span id="slide-graph">Graphical analysis is shown here....</span>
+				<div id="exam" onclick="hide1()" >Exam Given</div>
+					<span id="slide-exam">Contain the details about the exam given
+
+							<center>	<img src = "exam.jpg" height ="100px" width="100px"></center>
+					</span>
+
+				<div id="graph" onclick="hide3()">Graph Analysis</div>
+				<span id="slide-graph">Graphical analysis is shown here....
+					<center>	<img src = "graph.jpg" height ="100px" width="100px"> </center>
+					</span>
 		</nav>
-			
-			
-		<section id="details_data_display" >
-			
+
+
+		<section id="details_data_display" style="float:left;width:70%;height:80%;overflow:scroll;" >
+				<div id="exam_given"><?php include_once "exam_given.php"; ?>
+						<center>
+							<table>
+										<?php
+											echo "<tr>
+												<th>Exam Name</th>
+												<th>Personal score</th>
+												<th>Average score</th>
+												<th>Highest score</th>
+											</tr>";
+											//echo "$size_of_array";
+											for($i=0;$i<count($exam_name);$i++){
+												echo "<tr>
+														<td>$exam_name[$i]</td>
+														<td>$score_array[$i]</td>
+														<td>$average_array[$i]</td>
+														<td>$maximun_array[$i]</td>
+												</tr>";
+											}
+										?>
+							</table>
+						</center>
+
+				</div>
+
+				<br>
+				<div id="graph" style="height:90%;" > <?php include_once "Student_graph.php"; ?></div>
+
 		</section>
-		
-		
+
+
 	</body>
-	
+
 </html>
